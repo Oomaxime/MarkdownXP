201e6b98ece388a1b7f66a5192eeaf44df5f054a
Maxime <maxime.bidan33@gmail.com>
2024-10-26 16:39:32 +0200

    feat(webBrowser): add final feature and cleaning css

--- src/components/WebBrowser/WebBrowser.jsx
+++ src/components/WebBrowser/WebBrowser.jsx
@@ -1,12 +1,53 @@
+import { useRef, useState } from "react";
 import "./webBrowser.css";
+import PropTypes from "prop-types";
 
-export default function WebBrowser() {
+export default function WebBrowser({
+  title,
+  url,
+  leftOffset,
+  topOffset,
+  children,
+}) {
+  const browserRef = useRef(null);
+  const [isDragging, setIsDragging] = useState(false);
+  const [offset, setOffset] = useState({ x: 0, y: 0 });
+
+  const handleMouseDown = (e) => {
+    setIsDragging(true);
+    setOffset({
+      x: e.clientX - browserRef.current.getBoundingClientRect().left,
+      y: e.clientY - browserRef.current.getBoundingClientRect().top,
+    });
+  };
+
+  const handleMouseMove = (e) => {
+    if (isDragging) {
+      browserRef.current.style.left = `${e.clientX - offset.x}px`;
+      browserRef.current.style.top = `${e.clientY - offset.y}px`;
+    }
+  };
+
+  const handleMouseUp = () => {
+    setIsDragging(false);
+  };
+
   return (
-    <div className="browserWindow">
-      <div className="browserWindowTitleBar">
+    <div
+      className="browserWindow"
+      ref={browserRef}
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}
+      style={{
+        position: "absolute",
+        left: leftOffset || "100px",
+        top: topOffset || "100px",
+      }}
+    >
+      <div className="browserWindowTitleBar" onMouseDown={handleMouseDown}>
         <div className="mainTitle">
           <img src="./src/assets/explorerLogo.png" alt="IE" />
-          <span>Cool Music - Microsoft Internet Explorer</span>
+          <span>{title || "MarkdownXP Explorer"}</span>
         </div>
         <img
           src="./src/assets/closeButton.svg"
@@ -39,6 +80,7 @@ export default function WebBrowser() {
           <img src="./src/assets/winXPpicto.png" alt="WindowsXP" />
         </div>
       </div>
+      <hr className="styledHr" />
       <div className="adressBar">
         <p>
           A<span>d</span>dress
@@ -46,7 +88,7 @@ export default function WebBrowser() {
         <div className="urlInput">
           <div>
             <img src="./src/assets/iexplorePage.png" alt="IE page" />
-            <p>https://www.youtube.com/watch?v=BYpKQ65DugI</p>
+            <p>{url || "https://www.avoir20sur20.com/?=projetMarkdonwXP"}</p>
           </div>
           <img src="./src/assets/dropdownArrow.svg" alt="see more" />
         </div>
@@ -54,6 +96,7 @@ export default function WebBrowser() {
           <img src="./src/assets/greenArrow.svg" alt="go" />
           <p>Go</p>
         </div>
+        <hr className="styledHrV" />
         <div className="linkDiv">
           <p>Links</p>
           <img
@@ -63,22 +106,8 @@ export default function WebBrowser() {
           />
         </div>
       </div>
-      <div className="browserContent">
-        <div className="browserContentTitle">
-          <p>Portfolio de jecko</p>
-        </div>
-        <div className="browserContentText">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-            malesuada, purus a lacinia tincidunt, metus justo congue massa, nec
-            fermentum velit nunc non metus. Nulla facilisi. Nullam nec
-            consectetur nunc. Nulla facilisi. Nullam nec consectetur nunc. Nulla
-            facilisi. Nullam nec consectetur nunc. Nulla facilisi. Nullam nec
-            consectetur nunc.
-          </p>
-        </div>
-      </div>
-      <hr className="styledHr" />
+      <div className="browserContent">{children}</div>
+      <hr className="styledHrWithDropShadow" />
 
       <div className="browserFooter">
         <img src="./src/assets/iexplorePage.png" alt="IE page" />
@@ -87,3 +116,11 @@ export default function WebBrowser() {
     </div>
   );
 }
+
+WebBrowser.propTypes = {
+  title: PropTypes.string,
+  url: PropTypes.string,
+  children: PropTypes.node,
+  leftOffset: PropTypes.string,
+  topOffset: PropTypes.string,
+};
