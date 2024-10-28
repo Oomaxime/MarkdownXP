94c0af233cbd58e570d878a7880c40819c216c40
Maxime <maxime.bidan33@gmail.com>
2024-10-28 21:27:30 +0100

    merge(codebase): add markdown editor code V1

--- src/App.jsx
+++ src/App.jsx
@@ -1,157 +1,54 @@
-import { useState, useEffect } from "react";
-
-// Voir comment on va faire les routes, g juste mis ca pour tester le markdown
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MarkdownProvider from "./providers/MarkdownProvider";
-import MainLayout from "./components/MainLayout/MainLayout"
+import MainLayout from "./components/MainLayout/MainLayout";
 import Editor from "./components/Editor/Editor";
 import Preview from "./components/Preview/Preview";
-import NavBarApp from "./components/NavBarApp/NavBarApp"
-import BottomBar from "./components/BottomBar/BottomBar"
+import NavBarApp from "./components/NavBarApp/NavBarApp";
+import BottomBar from "./components/BottomBar/BottomBar";
 import Navbar from "./components/NavBar/Navbar";
 import WebBrowser from "./components/WebBrowser/WebBrowser";
-import EditorProvider from "./providers/EditorProvider"
-
-//Recuperation jour/mois pour l'eveneemnt historique
-function getDate(){
-  const today = new Date();
-  const month =today.getMonth();
-  const day = today.getDate();
-  //console.log("Date actuelle: ", { day, month });
-  return{ day, month};
-}
+import EditorProvider from "./providers/EditorProvider";
 
 function App() {
-  // Stackage citations, blagues, mocktails, recettes, evenement historique
-  const [ advice, setAdvice ] = useState('');
-  const [ joke, setJoke ] = useState ({ setup: '', punchline: ''});
-  const [ mocktail, setMocktail] = useState({strDrink:'', strDrinkThumb: ''});
-  const [ recipe, setRecipe] = useState({ strMeal:'', strInstructions:'', strIngredients:[]});
-  const [ randomEvent, setRandomEvent] = useState();
-  
-
-  //Recup Citation depuis API
-  function fetchAdvice() {
-    fetch('https://api.adviceslip.com/advice')
-    .then((res) => (res.json()))
-    .then((data) => setAdvice(data.slip.advice))
-    .catch((err) => console.error(err));    
-  }
-
-  //Recuperation Blagues depuis API
-  function fetchJock() {
-    fetch('https://official-joke-api.appspot.com/random_joke')
-    .then((res) => (res.json()))
-    .then ((data) => setJoke({ setup: data.setup, punchline: data.punchline}))
-    .catch((err) => console.error(err));
-  }
-
-  //Recup Mocktails depuis API
-  function fetchMocktail() {
-    fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic')
-    .then((res) => (res.json()))
-    .then((data) => { const randomMocktail = data.drinks[Math.floor(Math.random() * data.drinks.length)]; 
-      setMocktail({ strDrink: randomMocktail.strDrink, strDrinkThumb: randomMocktail.strDrinkThumb});})
-    .catch((err) => console.error(err));
-  }
-
-  //recuperer Recettes depuis API
-  function fetchRecipe() {
-    fetch('https://www.themealdb.com/api/json/v1/1/random.php')
-    .then((res) =>(res.json()))
-    .then((data) => {
-      const recipe = data.meals[0];
-      const ingredients = [];
-      for (let i = 1; i <= 20; i++){
-        const ingredient = recipe["strIngredient"+ i];
-        const measure = recipe["strMeasure" + i];
-
-        if(ingredient){
-          let ingredientMeasure = '';
-          if(measure){
-            ingredientMeasure = measure+ " " + ingredient;
-          } else {
-            ingredientMeasure = ingredient;
-          }
-          ingredients.push(ingredientMeasure);
-        }
-      }
-      setRecipe({
-        strMeal: recipe.strMeal, strInstructions: recipe.strInstructions, strIngredients: ingredients
-      });
-    })
-    .catch((err) => console.error(err));
-  }
-
-  //Recuperation Eveneemnts historique API
-  function fetchEventHistorical(){
-    const { day, month } = getDate();
-    fetch('https://history.muffinlabs.com/date/' + (month + 1) + '/' + day)
-    .then((res) =>(res.json()))
-    .then((data) => {
-      const events = data.data.Events;
-      if (events.length >0){
-        const randomEvent = Math.floor(Math.random()*events.length);
-        setRandomEvent(events[randomEvent]);
-      }})
-    .catch((err) => console.error(err));
-  }
-
-  useEffect(() => {
-    fetchAdvice();
-    fetchJock();
-    fetchMocktail();
-    fetchRecipe();
-    fetchEventHistorical();
-
-    const intervalId = setInterval(() => {fetchAdvice(); fetchJock(); fetchMocktail(); fetchRecipe(); fetchEventHistorical()}, 60000);
-    
-    return () =>  clearInterval(intervalId); 
-  }, [] );
-
-  function Quotes() {
+  function Desktop() {
     return (
-     <>
-      <main>
-        <WebBrowser />
-      </main>
-      <Navbar />
-    </>
-    )
+      <>
+        <main>
+          <WebBrowser />
+        </main>
+        <Navbar />
+      </>
+    );
   }
 
   function Markdown() {
     return (
       <div className="markdownApp">
         <EditorProvider>
-          <NavBarApp/>
+          <NavBarApp />
           <MarkdownProvider>
-              <MainLayout>
-                <MainLayout.Column>
-                    <Editor />
-                </MainLayout.Column>
-                <MainLayout.Column>
-                  <Preview />
-                </MainLayout.Column>
-              </MainLayout>
+            <MainLayout>
+              <MainLayout.Column>
+                <Editor />
+              </MainLayout.Column>
+              <MainLayout.Column>
+                <Preview />
+              </MainLayout.Column>
+            </MainLayout>
           </MarkdownProvider>
-          <BottomBar/>
+          <BottomBar />
         </EditorProvider>
       </div>
-
-    )
+    );
   }
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={Quotes()}>
-        </Route>
-        <Route path="/markdown" element={Markdown()}>
-        </Route>
+        <Route path="/" element={Desktop()}></Route>
+        <Route path="/markdown" element={Markdown()}></Route>
       </Routes>
     </BrowserRouter>
-)
+  );
 }
-
 
 export default App;
--- src/components/NavBarApp/NavBarApp.jsx
+++ src/components/NavBarApp/NavBarApp.jsx
@@ -1,52 +1,53 @@
-import "./NavBarApp.css"
-import iconeApp from '../../images/icone.png'
-import iconeClose from '../../images/close.png'
+import "./NavBarApp.css";
+import iconeApp from "../../assets/images/icone.png";
+import iconeClose from "../../assets/images/closeButton.svg";
 import DropdownMenu from "../DropdownMenu/DropdownMenu";
 import { useState } from "react";
 
-
-
 function NavBarApp() {
+  const [isHovered, setIsHovered] = useState(false);
+  const [parentPosition, setParentPosition] = useState(0);
 
-    const [isHovered, setIsHovered] = useState(false);
-    const [parentPosition, setParentPosition] = useState(0)
+  return (
+    <nav className="navBar">
+      <div className="moveBar">
+        <div className="nameProgram">
+          <img className="iconeProgram" src={iconeApp} alt="icone" />
+          <p>Titre du fichier - MarkdownXp.exe</p>
+        </div>
+        <img className="buttonClose" src={iconeClose} alt="icone" />
+      </div>
+      <div className="toolBar">
+        <a
+          href=""
+          onMouseEnter={() => {
+            setIsHovered(true);
+            setParentPosition(0);
+          }}
+          onMouseLeave={() => setIsHovered(false)}
+        >
+          File
+          {isHovered && parentPosition == 0 && (
+            <DropdownMenu Dict_link={{ test: "test", dasdasd: "adad" }} />
+          )}
+        </a>
 
-
-    return (
-        <nav className="navBar">
-            <div className="moveBar">
-                <div className="nameProgram">
-                    <img className="iconeProgram" src={iconeApp} alt="icone" />
-                    <p>Titre du fichier - MarkdownXp.exe</p>
-                </div>
-                <img className="buttonClose" src={iconeClose} alt="icone" />
-            </div>
-            <div className="toolBar">
-                <a href="" 
-                onMouseEnter={()=> {setIsHovered(true); setParentPosition(0)}}
-                onMouseLeave={()=> setIsHovered(false)}
-                >File
-
-                {isHovered && (parentPosition == 0) && (
-                    <DropdownMenu Dict_link={{test:'test', dasdasd:'adad'}}/>
-                )}
-                
-                </a>
-
-                
-                
-                <a href=""                
-                onMouseEnter={()=> {setIsHovered(true); setParentPosition(1)}}
-                onMouseLeave={()=> setIsHovered(false)}
-                >Edit
-                {isHovered && (parentPosition == 1) && (
-                    <DropdownMenu Dict_link={{test:'test'}}/>
-                )}
-
-                </a>
-            </div>
-        </nav>
-    );
+        <a
+          href=""
+          onMouseEnter={() => {
+            setIsHovered(true);
+            setParentPosition(1);
+          }}
+          onMouseLeave={() => setIsHovered(false)}
+        >
+          Edit
+          {isHovered && parentPosition == 1 && (
+            <DropdownMenu Dict_link={{ test: "test" }} />
+          )}
+        </a>
+      </div>
+    </nav>
+  );
 }
 
 export default NavBarApp;
