db11d847b12bf6815a108eb17e530cf8280d1d1c
Louis <louisdondey@gmail.com>
2024-11-02 16:39:48 +0100

    SaveFile - mostly done (no coms in css)

--- src/components/SaveFile/SaveFile.jsx
+++ src/components/SaveFile/SaveFile.jsx
@@ -4,10 +4,16 @@ import { useLocalStorageContext } from '../../providers/LocalStorageProvider';
 
 
 function SaveFile() {
+    // Custom hook to manage the markdown content and the title of the markdown
     const [markdown, setMarkdown, titleMarkdown, setTitleMarkdown] = useMarkdown();
+    // Custom hook to manage markdowns stored data and the id of the current one.
     const { data, setData, currentId, setCurrentId } = useLocalStorageContext();
 
     function Save() {
+        // Save the current markdown into the localstorage
+        // Check is the current id is set
+        // - true : find the corresponding object and add the modification (title and body)
+        // - false : create a new markdown in the local storage and set the current id to its newly created id
         if (currentId) {
             setData(data.map(file => {
                 if (file.id === currentId) {
